fb9c39f350153bdb292292ee3ddd8aeadbba4a80
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2024-06-03 02:06:23 -0700

    Add files via upload

---
+++ ModifiedFlowNet/occNerfDataset.py
@@ -0,0 +1,17 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self, input1_data, input2_data, label):
+        self.input1_data = input1_data # 1x18x300x300x24
+        self.input2_data = input2_data # 1x18x300x300x24
+        self.label = label
+
+    def __len__(self):
+        return len(self.label_data)
+
+    def __getitem__(self, idx):
+        input1 = self.input1_data[idx]
+        input2 = self.input2_data[idx]
+        label = self.label[idx]
+        return input1, input2
