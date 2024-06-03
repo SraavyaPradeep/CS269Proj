b6c538b68b2542b4388769060658359299f13011
Sraavya Pradeep <46724697+SraavyaPradeep@users.noreply.github.com>
2024-06-03 03:06:53 -0700

    Update occNerfDataset.py

--- ModifiedFlowNet/occNerfDataset.py
+++ ModifiedFlowNet/occNerfDataset.py
@@ -1,11 +1,11 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 
-class CustomDataset(Dataset):
+class OccNerfDataset(Dataset):
     def __init__(self, input1_data, input2_data, label):
         self.input1_data = input1_data # 1x18x300x300x24
         self.input2_data = input2_data # 1x18x300x300x24
-        self.label = label
+        self.label = label #1x2x300x300
 
     def __len__(self):
         return len(self.label_data)
@@ -14,4 +14,4 @@ class CustomDataset(Dataset):
         input1 = self.input1_data[idx]
         input2 = self.input2_data[idx]
         label = self.label[idx]
-        return input1, input2
+        return input1, input2, label
